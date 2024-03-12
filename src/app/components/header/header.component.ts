a9278dc9c08f60dfb25ef5da01d2931a7e9b991b
maksof-usama <smusamakhalid@gmail.com>
2024-03-12 10:56:16 +0500

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone:true
+  standalone:true,
+  imports: [NgClass, RouterLink, NgFor, NgIf],
 })
 export class HeaderComponent {
+  menus=[
+    {
+      lable:"About",
+      href:'/about',
+    },
+    {
+      lable:'Content',
+      href:"/content"
+    }
+  ]
 
 }
